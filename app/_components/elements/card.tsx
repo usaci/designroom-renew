84842c4576aec590b1bb12f130e27afa9d0eb9ae
usaci <a.akari300@gmail.com>
2024-04-27 19:38:09 +0900

    create-card-component

--- app/_components/elements/card.tsx
+++ app/_components/elements/card.tsx
@@ -1,18 +1,32 @@
-const cardLayout: Function = () => {
+"use client";
+import styles from "@/app/styles/card.module.scss"
+type CardProps = {
+    title: String,
+    date: String,
+    categories: Array<string>,
+}
+const cardLayout: Function = ({title, children, date, categories}: CardProps & {children: React.ReactNode}) => {
+
+    const getCategories = () => {
+        console.log(categories, title)
+    }
     return (
-        <li>
+        <li className={styles.card}>
             <article>
-                <div className="inner">    
-                    <img src="" alt="" />
-                    <ul className="categories">
-                        <li className="category">カテゴリ</li>
+                <div className={styles.inner}>    
+                    <figure className={styles.eyecatch}>
+                        <img src="dummy.jpg" alt="" />
+                    </figure>
+                    <ul className={styles.categories}>
+                        { categories && categories.map((category)=><li className={styles.category}>{ category }</li>) }
+                        {/* <li className={styles.category} onClick={getCategories}>カテゴリ</li> */}
                     </ul>
-                    <h2>ここにタイトルが入ります。ここにタイトルが入ります。</h2>
+                    <h2 className={styles.title}>{title}</h2>
+                    <time className={styles.postDate}>{date}</time>
                 </div>
             </article>
 
         </li>
     )
 }
-
 export default cardLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 import Image from "next/image";
 import styles from "./index.module.scss";
+import layoutStyles from "./styles/layout.module.scss";
 import Card from "./_components/elements/card"
 
 export default function Home() {
   return (
       <div className={styles.inner}>
-        <ul className={styles.cards}>
-          <Card></Card>
+        <ul className={layoutStyles.cards}>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']} date={'2024/04/01'}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
+          <Card title={"これはテストです。これはテストです。"} categories={['ああ', 'いい']}></Card>
         </ul>
       </div>
   );
