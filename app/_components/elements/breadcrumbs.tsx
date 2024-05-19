adb44dbc19868e0b6311aa8749054737cda4eabc
Usaci <97497875+usaci@users.noreply.github.com>
2024-05-19 21:37:35 +0900

    className修正

--- app/_components/elements/breadcrumbs.tsx
+++ app/_components/elements/breadcrumbs.tsx
@@ -1,14 +1,10 @@
 'use client';
 import Styles from '@/app/styles/breadcrumbs.module.scss';
 import React, { useEffect, useState } from 'react';
-import { usePathname, } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 
-type BreadProps = {
-    href: string,
-    title: string
-}
 
-const breadcrumbs: React.FC = () => {
+const Breadcrumbs: React.FC = () => {
     const path = usePathname()
     const pattern_post = /\/post\/\w+/;
     const pattern_category = /\/category\/\w+/;
@@ -27,14 +23,14 @@ const breadcrumbs: React.FC = () => {
             return (
                 <div className={Styles.breadcrumbs}>
                     <ul>
-                        {<li><a href="/" className="link">ホーム</a></li>}
-                        {(path.match(pattern_posts)) && (<li><a href="/post" className="link">記事</a></li>)}
-                        {path.match(pattern_category) && (<li><a href={path} className="link">{siteTitle}</a></li>)}
-                        {path.match(pattern_post) && (<li><a href={path} className="link">{siteTitle}</a></li>)}
+                        {<li><a href="/" className="link" key="breadcrumbs__home">ホーム</a></li>}
+                        {(path.match(pattern_posts)) && (<li><a href="/post" className="link" key="breadcrumbs__post">記事</a></li>)}
+                        {path.match(pattern_category) && (<li><a href={path} className="link" key="breadcrumbs__cat">{siteTitle}</a></li>)}
+                        {path.match(pattern_post) && (<li><a href={path} className="link" key={"breadcrumbs__" + siteTitle}>{siteTitle}</a></li>)}
                     </ul>
                 </div>
             )
         }
     }
 }
-export default breadcrumbs;
+export default Breadcrumbs;
